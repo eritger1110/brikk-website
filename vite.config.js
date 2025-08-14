5cb6fba713c4722099c1cc16b410eee941126856
eritger1110 <eric.ritger@gmail.com>
2025-08-14 14:13:29 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react( )],
   server: {
     host: '0.0.0.0',
     port: 5173
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173
+    port: 10000
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom']
-        }
-      }
-    }
-  },
-  base: '/'
+    outDir: 'dist'
+  }
 })
-
